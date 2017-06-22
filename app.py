04dce0ccca4b371f1d61b07ba7cc09df694a5c05
Jithu R Jacob <jithurjacob@gmail.com>
2017-06-22 19:27:56 +0530

    removed flask cors

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route('/')
 def index():
