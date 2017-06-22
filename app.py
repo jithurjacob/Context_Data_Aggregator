493be8821ae742596911a358c047821043d9bcc5
Jacob <540607@cognizant.com>
2017-06-22 16:06:48 +0530

    removed spaces test

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
+ 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
